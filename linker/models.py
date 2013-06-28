8aeacc2cd7926ea76f8e00f5f0e347c0b9a8da64
liaosiwei <liaosiweiorxiaowei@gmail.com>
2013-06-28 22:40:11 +0800

    add metro style

--- linker/models.py
+++ linker/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-
+    
 class Container(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True)
     name = models.CharField(max_length=40, null=True, blank=True)
     tags = models.CharField(max_length=100, null=True, blank=True)
     
